a4d2f3ceca93029a4f6b4b0424f798c3fe4d3c17
rob phillips <rob@robphillips.me>
2026-01-26 22:06:29 -0500

    [shadcn] turbopack hates inline-flex and svgs

--- packages/ui/src/components/ui/button.tsx
+++ packages/ui/src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cn } from '../../lib/utils'
 import { type VariantProps, cva } from 'class-variance-authority'
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+  "flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all cursor-pointer disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
   {
     variants: {
       variant: {
